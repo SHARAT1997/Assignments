a8d322107b4f3d1968f7b5d11253f336b4ca1e8a
SHARAT BHAT <96580387+SHARAT1997@users.noreply.github.com>
2022-07-15 08:59:24 +0530

    Add files via upload

---
+++ Assignments/a13-07-2022/inc/mult.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include <sys/wait.h>
+
+#include <fstream>
+
+#include <string>
+
+#include <cstring>
+
+#include <vector>
+
+#include <stdio.h>
+
+#define PATH "/home/sharatbhat/git1/SystemProgram/CapGBatchJune06/clswk/a13-07-2022/data"
+
+using namespace std;
